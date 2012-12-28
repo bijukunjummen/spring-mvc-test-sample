02495d0c74d9dbf24729ce866ddc303963389986
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2012-12-28 16:49:28 -0500

    Added endpoint controller

---
+++ src/main/java/mvcsample/web/EndpointDocController.java
@@ -0,0 +1,27 @@
+package mvcsample.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@Controller
+@RequestMapping("/rootendpoint")
+public class EndpointDocController {
+
+	private final RequestMappingHandlerMapping handlerMapping;
+
+	@Autowired
+	public EndpointDocController(RequestMappingHandlerMapping handlerMapping) {
+		this.handlerMapping = handlerMapping;
+	}
+	
+	@RequestMapping(value="/endpointdoc", method=RequestMethod.GET)
+	public String show(Model model) {
+		model.addAttribute("handlerMethods", this.handlerMapping.getHandlerMethods());
+		return "endpointdoc";
+	}
+	
+}
