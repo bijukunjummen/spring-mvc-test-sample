5271a95aacf056fca2b780897e6d8b640acc31b7
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2014-03-13 21:56:53 -0400

    Working version

--- src/main/java/mvcsample/config/CustomWebAppInitializer.java
+++ src/main/java/mvcsample/config/CustomWebAppInitializer.java
@@ -7,28 +7,28 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 import javax.servlet.Filter;
 
-public class CustomWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
+public class CustomWebAppInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] {RootConfiguration.class};
+		return new Class[] { RootConfiguration.class };
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class[] {MvcConfiguration.class};
+		return new Class[] { MvcConfiguration.class };
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		return new String[]{"/"};
+		return new String[] { "/" };
 	}
 
-    @Override
-    protected Filter[] getServletFilters() {
-        return new Filter[]{new HiddenHttpMethodFilter(), new MultipartFilter(), new OpenEntityManagerInViewFilter()};
-    }
-
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] { new HiddenHttpMethodFilter(),
+				new MultipartFilter(), new OpenEntityManagerInViewFilter() };
+	}
 
 }
--- src/main/java/mvcsample/config/MvcConfiguration.java
+++ src/main/java/mvcsample/config/MvcConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.stereotype.Controller;
@@ -19,12 +20,10 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles2.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles2.TilesView;
 
-
 @EnableWebMvc
 @EnableSpringDataWebSupport
-@ComponentScan(basePackages="mvcsample.web", useDefaultFilters=false, includeFilters=@Filter(type=FilterType.ANNOTATION, value=Controller.class))
+@ComponentScan(basePackages = "mvcsample.web", useDefaultFilters = false, includeFilters = @Filter(type = FilterType.ANNOTATION, value = Controller.class))
 public class MvcConfiguration extends WebMvcConfigurerAdapter {
-
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
@@ -34,44 +33,51 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/resources/**").addResourceLocations(
+				"/resources/");
 	}
 
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+	public void configureDefaultServletHandling(
+			DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
-	
+
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
 	}
-	
+
 	@Bean
 	public MessageSource messageSource() {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasenames("WEB-INF/i18n/messages","WEB-INF/i18n/application");
+		messageSource.setBasenames("WEB-INF/i18n/messages",
+				"WEB-INF/i18n/application");
 		return messageSource;
 	}
 
 	@Bean
+	@Order(1)
 	public UrlBasedViewResolver tilesViewResolver() {
 		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
 		viewResolver.setViewClass(TilesView.class);
 		return viewResolver;
 	}
-	
+
 	@Bean
 	public TilesConfigurer tilesConfigurer() {
 		TilesConfigurer tilesConfigurer = new TilesConfigurer();
-		tilesConfigurer.setDefinitions(new String[]{"/WEB-INF/layouts/layouts.xml", "/WEB-INF/views/**/views.xml" });
+		tilesConfigurer
+				.setDefinitions(new String[] { "/WEB-INF/layouts/layouts.xml",
+						"/WEB-INF/views/**/views.xml" });
 		return tilesConfigurer;
 	}
 
-    @Bean
-    public InternalResourceViewResolver jspViewResolver() {
-        InternalResourceViewResolver jspViewResolver = new InternalResourceViewResolver();
-        jspViewResolver.setPrefix("/WEB-INF/pages/");
-        jspViewResolver.setSuffix(".jsp");
-        return jspViewResolver;
-    }
+	@Bean
+	@Order(2)
+	public InternalResourceViewResolver jspViewResolver() {
+		InternalResourceViewResolver jspViewResolver = new InternalResourceViewResolver();
+		jspViewResolver.setPrefix("/WEB-INF/pages/");
+		jspViewResolver.setSuffix(".jsp");
+		return jspViewResolver;
+	}
 }
